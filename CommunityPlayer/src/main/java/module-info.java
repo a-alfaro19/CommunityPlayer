c032556d2447de9bb9ed4710af64988c99987f7d
a-alfaro19 <a.alfaro@estudiantec.cr>
2024-02-24 08:19:29 -0600

    Revert "[ADD] Un proyecto JavaFX desde cero"
    This reverts commit b417ec07cd6f214584ac120c3c07716097466eac.

--- CommunityPlayer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.communityplayer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.communityplayer to javafx.fxml;
-    exports com.example.communityplayer;
-}
